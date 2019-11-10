951c3d77fe820bd9696c58f0ccf44455ccdade69
Daniel Bonniot <bonniot@gmail.com>
2019-11-10 09:44:40 +0100

    Update fire attack sprite and scale to range

--- src/main/java/view/FireTowerView.java
+++ src/main/java/view/FireTowerView.java
@@ -6,15 +6,17 @@ import com.codingame.gameengine.module.tooltip.TooltipModule;
 
 import TowerDefense.Attacker;
 import TowerDefense.Tower;
+import TowerDefense.TowerProperty;
 
 public class FireTowerView extends TowerView {
 
 	public FireTowerView(Tower tower, Group boardGroup, GraphicEntityModule graphics, TooltipModule tooltips) {
 		super(tower, boardGroup, graphics, tooltips, "fireTower");
 		towerFixedSprite = Utils.createTowerSprite(graphics, "fireTowerFixed" + upgradeLevel + ".png", tower.getTile().getX(), tower.getTile().getY());
-		attackSprite = graphics.createSprite().setImage("fireTowerAttack.png").setAlpha(0).setScale(3);
-		attackSprite.setX(BoardView.CELL_SIZE * tower.getTile().getX() - BoardView.CELL_SIZE);
-		attackSprite.setY(BoardView.CELL_SIZE * tower.getTile().getY() - BoardView.CELL_SIZE);
+		attackSprite = graphics.createSprite().setImage("fireTowerAttack.png").setAlpha(0).setScale(tower.getProperty(TowerProperty.RANGE));
+		attackSprite.setAnchor(0.5);
+		attackSprite.setX(BoardView.CELL_SIZE * tower.getTile().getX() + BoardView.CELL_SIZE / 2);
+		attackSprite.setY(BoardView.CELL_SIZE * tower.getTile().getY() + BoardView.CELL_SIZE / 2);
 		commitSprites();
 		updateTooltip();
 	}
@@ -24,5 +26,11 @@ public class FireTowerView extends TowerView {
 		attackSprite.setAlpha(1);
 		graphics.commitEntityState(0, attackSprite);
 		attackSprite.setAlpha(0);
+	}
+	
+	@Override
+	public void upgrade() {
+		super.upgrade();
+		attackSprite.setScale(tower.getProperty(TowerProperty.RANGE));
 	}
 }
